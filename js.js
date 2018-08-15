d1961fa7803a0c5473c844109c02f80ce7f21008
Ryan Bachman <jacksprat455@gmail.com>
2018-08-15 10:42:28 -0500

    Selectors

--- js.js
+++ js.js
@@ -1,23 +1,43 @@
 /* Style the HTML elements according to the following instructions.
-DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
+DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY JavaScript!
 */
 
 
 /* Give the <body> element a background of #bdc3c7*/
+var body = document.getElementsByTagName("body");
+body[0].style.backgroundColor="#bdc3c7";
 
 /* Make the <h1> element #9b59b6*/
+var geth1Element = document.getElementsByTagName("h1");
+geth1Element[0].style.color = "#9b59b6";
 
 
 /* Make all <h2> elements orange */
-
+var geth2Elements = document.getElementsByTagName("h2");
+geth2Elements[0].style.color = "orange";
 
 /* Make all <li> elements blue(pick your own hexadecimal blue)*/
+var getliElements = document.getElementsByTagName("li");
+for(var i = 0; i<getliElements.length; i++)
+{
+    getliElements[i].style.color = "blue";
+}
 
 
 /*Change the background on every paragraph to be yellow*/
+var getpElements = document.getElementsByTagName("p");
+for(var x = 0; x<getpElements.length; x++)
+{
+    getpElements[x].style.backgroundColor = "yellow";
+}
 
 
 /*Make all inputs have a 3px red border*/
+var getinputElements = document.getElementsByTagName("input");
+for(var y = 0; i<getinputElements.length; y++){
+    
+}
+
 
 
 /* Give everything with the class 'hello' a white background*/
